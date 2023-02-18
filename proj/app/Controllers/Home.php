a9b603d596f82cdfee266d5eed88ec13066ba899
ampm <>
2023-02-18 17:29:21 +0000

    Welcome page and pages related to pages controller converted to latte template.

--- proj/app/Controllers/Home.php
+++ proj/app/Controllers/Home.php
@@ -6,6 +6,8 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message', ['title' => 'Welcome to CI 4']);
+        $title = 'Welcome to CI 4';
+        // return view('welcome_message', ['title' => 'Welcome to CI 4']);
+        return $this->renderLatte(view: 'welcome_message', params: compact('title'));
     }
 }
--- proj/app/Controllers/Pages.php
+++ proj/app/Controllers/Pages.php
@@ -8,18 +8,21 @@ class Pages extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        // return view('welcome_message');
+        return $this->renderLatte(view: 'welcome_message');
     }
 
     public function show($page = 'home')
     {
-        if (!is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+        if (!is_file(APPPATH . 'Views/pages/' . $page . '.latte')) {
             // Whoops, we don't have a page for that!
             throw new PageNotFoundException($page);
         }
 
         $data['title'] = ucfirst($page); // Capitalize the first letter
 
-        return  view('pages/' . $page, $data);
+        // return  view('pages/' . $page, $data);
+
+        return $this->renderLatte(view: "pages/{$page}", params: $data);
     }
 }
